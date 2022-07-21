1c6b338229c0d99b6eac4661e06e901cff9ec5f0
CarlosWGama <carloswgama@gmail.com>
2022-07-21 17:16:33 -0300

    Animação

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
       
     public float velocidade = 4f; //Valor padrão inicial
+    private Animator animator;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
 
     void Update() {
+
+        //Altera a animação
+        animator.SetBool("walking", Input.GetButton("Horizontal") || Input.GetButton("Vertical"));
+        
         //Horizontal
         if (Input.GetAxisRaw("Horizontal") > 0) {
             transform.localScale = new Vector3(1, 1, 1);
