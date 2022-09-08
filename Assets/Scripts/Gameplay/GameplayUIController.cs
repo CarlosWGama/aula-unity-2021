cfbf85472b7f21db885c35388e9d0ab4c691b159
CarlosWGama <carloswgama@gmail.com>
2022-09-08 19:42:43 -0300

    Save

--- Assets/Scripts/Gameplay/GameplayUIController.cs
+++ Assets/Scripts/Gameplay/GameplayUIController.cs
@@ -7,7 +7,10 @@ using TMPro;
 public class GameplayUIController : MonoBehaviour {
 
     public TextMeshProUGUI pontos;
+    public TextMeshProUGUI record;
+
     private int pontosAtuais = 0;
+    private int maiorPontuacao = 0;
     public Slider vida;
     public GameObject gameOver;
     private PlayerController player;
@@ -19,6 +22,9 @@ public class GameplayUIController : MonoBehaviour {
         vida.maxValue = 10;
         if (BGM != null)
             AudioController.Instance.PlayBGM(BGM);
+        //Caso não exista record, pega o valor padrão (0)
+        maiorPontuacao = PlayerPrefs.GetInt("record", 0); 
+        record.text = maiorPontuacao.ToString();
     }
 
     void Update() {
@@ -27,11 +33,18 @@ public class GameplayUIController : MonoBehaviour {
 
     public void GanharPontos() {
         pontosAtuais++;
+        if (pontosAtuais > maiorPontuacao) {
+            maiorPontuacao = pontosAtuais;
+            record.text = maiorPontuacao.ToString();
+            PlayerPrefs.SetInt("record", maiorPontuacao);
+        } 
+
         pontos.text =  pontosAtuais.ToString();
     }
 
     public void GameOver() {
         gameOver.SetActive(true);
+        PlayerPrefs.Save();
     }
 
     public void Restart() {
